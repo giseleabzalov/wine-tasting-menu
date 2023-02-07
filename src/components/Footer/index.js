e71593fecb35f81993d4e5035330ca8363a2e876
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-07 17:02:03 -0500

    practice useEffect

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+export default function() {
+    return(
+        <footer>
+        <p><small><a href="https://github.com" rel="nonreferrer" target="_blank">See Code in GitHub</a></small></p>
+        </footer>
+    )
+}
